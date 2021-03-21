035929cfe92204232f712d8083ec8c8d001ab2f9
startec-official <gponon2000@gmail.com>
2021-03-21 12:12:58 -0400

    added code for scanner

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner, BarcodeScanResult } from '@ionic-native/barcode-scanner/ngx';
+import { UiService } from '../utils/ui.service';
 
 @Component({
   selector: 'app-scanner',
@@ -7,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScannerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private barcodeScanner: BarcodeScanner, private uiService: UiService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  scanQrCode(): void {
+    this.barcodeScanner.scan({
+      prompt: "Aim the box at the QR Code to scan...."
+    }).then((barcodeData: BarcodeScanResult) => {
+      if (barcodeData.cancelled) {
+        this.uiService.presentToast('scan cancelled...', 500);
+      }
+      else {
+        this.onQRCodeRead(barcodeData.text);
+      }
+    }).catch(error => {
+      this.uiService.presentToast('this scanner encountered an error. restart your app...', 1000);
+    });
+  }
+
+  onQRCodeRead(barcodeData: string) {
+
+  }
 
 }
