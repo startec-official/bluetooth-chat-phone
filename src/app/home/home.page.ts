b63fb5a06f0119454a8eee03394a49dd9c150cc6
startec-official <gponon2000@gmail.com>
2021-03-27 10:09:14 -0400

    added activate bluetooth button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BluetoothSerialService } from '../utils/bluetooth-serial.service';
+import { UiService } from '../utils/ui.service';
 
 @Component({
   selector: 'app-home',
@@ -10,19 +11,20 @@ export class HomePage implements OnInit {
 
   sendData: string;
 
-  constructor(private btSerial: BluetoothSerialService) {}
+  constructor(private btSerial: BluetoothSerialService, private uiService: UiService) {}
   
   ngOnInit(): void {
     this.sendData = "";
-    this.activateBluetooth();
   }
 
   activateBluetooth() {
     this.btSerial.activateBluetooth('B8:27:EB:3C:63:B4').subscribe(_ => {
       console.log('connected to bluetooth!');
+      this.uiService.presentToast('connected to Raspberry Pi!', 500);
     },
     error => {
       console.log(`activate bluetooth encountered an error: ${error}`);
+      this.uiService.presentToast(`activate bluetooth encountered an error: ${error}`,1000);
     });
   }
 
